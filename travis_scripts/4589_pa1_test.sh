9d4ee8c47aafc545fd0bc61a0af44c2a80219a92
Hanzhang Bai <hbai@buffalo.edu>
2020-10-04 07:22:36 -0400

    Update Travis CI script to reflect changes made

--- travis_scripts/4589_pa1_test.sh
+++ travis_scripts/4589_pa1_test.sh
@@ -2,27 +2,11 @@
 
 # This script is for testing python script only!
 
-# Change it to YOUR UBITName and your full legal name.
-ubitname="abcde"
-fullname="AFFF"
-semester="Fall_2020"
-personNumber="50308888"
-debug="" # Put anything inside "" to set it to be true
-timeout=190
-quiet="" # Put anything inside "" to set it to be true
-# End of personalisation section
 pathofpythonscript="./unit_tests/CSE489/testTemplate.py"
 pathofbinary="./unit_tests/CSE489/testTemplate_bin"
 filename="${ubitname}_pa1.tar"
 csvlocation="./unit_tests/CSE489/PA1.csv"
-version_number="1.4.4_PA1_opensource"
-export ubitname
-export fullname
-export semester
-export debug
-export timeout
-export quiet
-export personNumber
+version_number="Travis CI test"
 export version_number
 
 python3 $pathofpythonscript prompt
@@ -40,19 +24,4 @@ echo "Make sure UBITname, semester are configured!"
 echo "Fail for binary version"
 exit 1
 fi
-# Compile python binary is not going to be tested since the directory is hard coded
-# python3 $pathofpythonscript autocompile
-# if [ $? -ne 0 ]; then
-# echo "Make sure pre-requisites are installed!"
-# echo "Fail for autocompile"
-# exit 1
-# fi
-# Launcher
-unset ubitname
-unset fullname
-unset semester
-unset debug
-unset timeout
-unset quiet
-unset personNumber
 unset version_number
