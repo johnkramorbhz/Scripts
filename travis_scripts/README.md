1792a60db81108477e383996566acb7303c87648
Hanzhang Bai <johnkramerbhz@gmail.com>
2022-03-01 17:41:03 -0500

    Remove travis.yml

--- travis_scripts/README.md
+++
@@ -1,3 +0,0 @@
-# Scripts/scripts
-
-These are unit tests for unit test scripts. Do NOT use them as they are designed for Travis CI ONLY.
--- travis_scripts/independent_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pyFilePath="./unit_tests/CSE489/testTemplate.py"
-echo "INFO: running python script"
-python3 $pyFilePath version
-echo "INFO: checking revision"
-python3 $pyFilePath revision
